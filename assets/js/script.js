4f4247a25c47947af3e0b693c84a37b9f22be0e8
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-02-25 18:14:49 +0600

    dynamic card system completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,32 @@
-let cartArr = [{}];
+let cartArr = [];
 let cartCounter = 0;
 let cartClick = document.getElementsByClassName('cart_added');
+cards = "";
+
+document.getElementById('cart_goto').addEventListener('click', function () {
+    document.getElementById('cart_details').style.display = 'block';
+    cards = ""
+    for (i of cartArr) {
+        cards += `<div class="d-flex product_card justify-content-center"> <div class="customize_card"> <div class="d-flex align-items-center gap-3"> <div><img src="${i.image}" alt="" class="checkOutImg"></div> <div class="textSize">${i.phoneSelected}</div> </div> <div class="d-flex align-items-center"><span class="increment">+</span> <input type="number" class="productNumber" value="${i.productCount}" readonly> <span class="decrement">-</span> <div class="priceDiv">$<span class="checkoutPrice">${(i.productCount) * (i.phonePrice)}</span></div> <span class="remove">X</span> </div> </div> </div>`
+    }
+    document.getElementById('cart_details').innerHTML = cards;
+
+})
+
 
 for (let i = 0; i < cartClick.length; i++) {
     cartClick[i].addEventListener('click', cart_operation); // Removed '()' from cart_operation
 }
 
 function cart_operation(event) {
-    console.log(event.target.innerText);
     const card = event.target.parentElement.parentElement.parentElement;
     const price = card.querySelector('.price').innerText;
     const phoneName = card.querySelector('.brand_name').innerText;
+    const imgSrc = card.querySelector('.imgSrc').src;
     let createdObject = {
         phoneSelected: phoneName,
         phonePrice: price,
+        image: imgSrc,
         productCount: 1,
     }
 
@@ -21,15 +34,20 @@ function cart_operation(event) {
     let index = checkObjectInArray(phoneName);
     if (index === -1) {
         cartArr.push(createdObject);
-        console.log("If Executed with", index)
     }
     else {
         cartArr[index].productCount++;
-        console.log("else Executed with", index)
     }
 
     //Here I Update the value of  cart product
-    cartCounter = cartArr.length - 1;
+    cartCounter = cartArr.length;
+    if (cartCounter >= 1) {
+        document.getElementById('cart_goto').setAttribute("href", "#cart_details");
+
+    }
+    else {
+        document.getElementById('cart_details').style.display = 'none';
+    }
     document.getElementById('total_cart').innerText = cartCounter;
 }
 
@@ -37,3 +55,5 @@ function checkObjectInArray(phoneName) {
     return cartArr.findIndex(index => index.phoneSelected === phoneName);
 
 }
+
+
