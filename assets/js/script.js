6b08fbc68e5f60c0e50b8bc59b8a0706611e8a1b
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-02-25 15:27:41 +0600

    cart add logic completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,39 @@
-let cart_arr = [];
-let cart_click = document.getElementsByClassName('cart_added');
+let cartArr = [{}];
+let cartCounter = 0;
+let cartClick = document.getElementsByClassName('cart_added');
 
-for (let i = 0; i < cart_click.length; i++) {
-    cart_click[i].addEventListener('click', cart_operation); // Removed '()' from cart_operation
+for (let i = 0; i < cartClick.length; i++) {
+    cartClick[i].addEventListener('click', cart_operation); // Removed '()' from cart_operation
 }
 
 function cart_operation(event) {
     console.log(event.target.innerText);
-    cart_arr.push({});
-    // console.log(cart_click[i].parentElement.parentElement);
-
-    const card = event.target.parentElement.parentElement; // 'event.target' instead of 'cart_click[i]'
+    const card = event.target.parentElement.parentElement.parentElement;
     const price = card.querySelector('.price').innerText;
-    console.log(price);
+    const phoneName = card.querySelector('.brand_name').innerText;
+    let createdObject = {
+        phoneSelected: phoneName,
+        phonePrice: price,
+        productCount: 1,
+    }
+
+    //bellow I use if  if else condition to check are the object present in the array or not
+    let index = checkObjectInArray(phoneName);
+    if (index === -1) {
+        cartArr.push(createdObject);
+        console.log("If Executed with", index)
+    }
+    else {
+        cartArr[index].productCount++;
+        console.log("else Executed with", index)
+    }
+
+    //Here I Update the value of  cart product
+    cartCounter = cartArr.length - 1;
+    document.getElementById('total_cart').innerText = cartCounter;
+}
+
+function checkObjectInArray(phoneName) {
+    return cartArr.findIndex(index => index.phoneSelected === phoneName);
+
 }
