94f14c7535d34800d5bfce3e2554e3270c8dd891
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-02-26 10:50:06 +0600

    remove section completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,44 @@ let cartArr = [];
 let cartCounter = 0;
 let cartClick = document.getElementsByClassName('cart_added');
 cards = "";
+let removeProduct = document.getElementsByClassName('remove');
 
 document.getElementById('cart_goto').addEventListener('click', function () {
+    showcart();
+    // console.log(removeProduct.length)
+})
+function showcart() {
     document.getElementById('cart_details').style.display = 'block';
+
     cards = ""
     for (i of cartArr) {
-        cards += `<div class="d-flex product_card justify-content-center"> <div class="customize_card"> <div class="d-flex align-items-center gap-3"> <div><img src="${i.image}" alt="" class="checkOutImg"></div> <div class="textSize">${i.phoneSelected}</div> </div> <div class="d-flex align-items-center"><span class="increment">+</span> <input type="number" class="productNumber" value="${i.productCount}" readonly> <span class="decrement">-</span> <div class="priceDiv">$<span class="checkoutPrice">${(i.productCount) * (i.phonePrice)}</span></div> <span class="remove">X</span> </div> </div> </div>`
+        cards += `<div class="d-flex product_card justify-content-center"> <div class="customize_card"> <div class="d-flex align-items-center gap-3"> <div><img src="${i.image}" alt="" class="checkOutImg"></div> <div class="textSize">${i.phoneSelected}</div> </div> <div class="d-flex align-items-center"><span class="increment">+</span> <input type="number" class="productNumber" value="${i.productCount}" readonly> <span class="decrement">-</span> <div class="priceDiv">$<span class="checkoutPrice">${(i.productCount) * (i.phonePrice)}</span></div> <span class="remove" onclick="deleteCartProduct('${i.phoneSelected}')">X</span> </div> </div> </div>`
     }
     document.getElementById('cart_details').innerHTML = cards;
-
-})
+    cartCounter = cartArr.length;
+    document.getElementById('total_cart').innerText = cartCounter;
+    // productPriceInvoice();
+}
 
 
 for (let i = 0; i < cartClick.length; i++) {
-    cartClick[i].addEventListener('click', cart_operation); // Removed '()' from cart_operation
+    cartClick[i].addEventListener('click', cart_operation);
+
 }
+
+const deleteCartProduct = (product) => {
+    // console.log(product)
+    indexOfDeleteProduct = (checkObjectInArray(product));
+    cartArr.splice(indexOfDeleteProduct, 1);
+    showcart();
+
+}
+
+
+// function deleteProduct(ev) {
+//     const clickedElement = ev.target;
+//     console.log(clickedElement);
+// }
 
 function cart_operation(event) {
     const card = event.target.parentElement.parentElement.parentElement;
